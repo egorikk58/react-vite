9ba9ce7342990a9ba129dfa94e6250462ff28470
egorikk58 <egorka.andreev765@gmail.com>
2025-01-31 22:02:20 +0300

    made registry and login

--- src/components/auth/register.tsx
+++ src/components/auth/register.tsx
@@ -1,9 +1,47 @@
 import {Label} from "@/components/ui/label.tsx";
 import {Input} from "@/components/ui/input.tsx";
 import {Button} from "@/components/ui/button.tsx";
-import {NavLink} from "react-router";
+import {IRegisterRequest} from "@/api/auth/types.ts";
+import {useState} from "react";
+import {NavLink,useNavigate} from "react-router";
+import api from "@/api";
 
 const Register = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordVerify, setPasswordVerify] = useState("");
+    const [role, setRole] = useState(0);
+    const [error, setError] = useState<any>(null);
+    const navigate = useNavigate();
+
+    const handleCreateAccount = async () => {
+        if (!email || !password) {
+            setError("Введены некорректные данные!");
+        } else if (password !== passwordVerify) {
+            setError("Пароли должны совпадать!");
+        } else {
+            try {
+                const data: IRegisterRequest = {
+                    email,
+                    password,
+                    role: role ? "author" : "reader",
+                }
+
+                const request = await api.auth.register(data);
+
+                if (request.status === 200) {
+                    localStorage.setItem("accessToken", request.data.accessToken);
+                    localStorage.setItem("refreshToken", request.data.refreshToken);
+                    localStorage.setItem("email", email);
+                    navigate("/main");
+                }
+            } catch (error: any) {
+                console.error(error);
+                setError("Неверный формат почты или пароля!");
+            }
+        }
+    }
+
     return (
         <div className={"bg-slate-50 w-screen h-screen flex items-center justify-center"}>
             <div className={"w-[416px] box-border p-4 bg-white rounded-2xl flex flex-col items-start gap-4"}>
@@ -12,15 +50,15 @@ const Register = () => {
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                     <Label htmlFor="email">Почта</Label>
-                    <Input type="email" id="email" placeholder="Введите почту" />
+                    <Input type="email" id="email" placeholder="Введите почту" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                     <Label htmlFor="email">Пароль</Label>
-                    <Input type="email" id="email" placeholder="Введите пароль" />
+                    <Input type="password" id="password" placeholder="Введите пароль" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <Label htmlFor="email">Повторите пароль</Label>
-                    <Input type="email" id="email" placeholder="Повторите пароль" />
+                    <Label htmlFor="email" >Повторите пароль</Label>
+                    <Input type="password" id="password" placeholder="Повторите пароль" value={passwordVerify} onChange={(e) => setPasswordVerify(e.target.value)}/>
                 </div>
                 <div>
                     Выберите роль
@@ -29,8 +67,11 @@ const Register = () => {
                         <Button variant={"ghost"} className={"bg-transparent border-transparent"}>Автор</Button>
                     </div>
                 </div>
+                {error && <Label className={"text-md text-red-600"}>
+                    {error}
+                </Label>}
                 <div className={"w-full"}>
-                    <Button className="w-full h-10 bg-[#0F172A] text-white">Создать аккаунт</Button>
+                    <Button className="w-full h-10 bg-[#0F172A] text-white" onClick={handleCreateAccount}>Создать аккаунт</Button>
                 </div>
                 <div>
                     Уже есть аккаунт? <NavLink className={"text-indigo-500"} to={"/auth/login"}>Войти</NavLink>
