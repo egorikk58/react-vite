ceccc713e3fab02a7076429593ca6617a28d46df
egorikk58 <egorka.andreev765@gmail.com>
2025-02-02 20:25:15 +0300

    bug fix

--- src/api/post/index.ts
+++ src/api/post/index.ts
@@ -1,3 +1,4 @@
+// api/post/index.ts
 import { axiosClient } from "@/api/axiosConfig";
 import apiRoutes from "@/api/routes";
 import { AxiosPromise } from "axios";
@@ -5,7 +6,7 @@ import {
   IPost,
   ICreatePostRequest,
   IUpdatePostRequest,
-  IUpdatePostStatusRequest,
+  IUpdatePostStatusRequest
 } from "./types";
 
 export const getPosts = (): AxiosPromise<IPost[]> =>
@@ -15,30 +16,30 @@ export const createPost = (data: ICreatePostRequest): AxiosPromise<IPost> =>
   axiosClient.post(apiRoutes.POST.CREATE, data);
 
 export const updatePost = (
-  postId: number,
+  postId: number, 
   data: IUpdatePostRequest
 ): AxiosPromise<IPost> =>
   axiosClient.put(apiRoutes.POST.UPDATE(postId), data);
 
 export const updatePostStatus = (
-  postId: number,
+  postId: number, 
   data: IUpdatePostStatusRequest
 ): AxiosPromise<IPost> =>
   axiosClient.patch(apiRoutes.POST.STATUS(postId), data);
 
 export const uploadPostImage = (
-  postId: number,
+  postId: number, 
   image: File
 ): AxiosPromise<void> => {
   const formData = new FormData();
   formData.append("image", image);
   return axiosClient.post(apiRoutes.POST.IMAGES(postId), formData, {
-    headers: { "Content-Type": "multipart/form-data" },
+    headers: { "Content-Type": "multipart/form-data" }
   });
 };
 
 export const deletePostImage = (
-  postId: number,
+  postId: number, 
   imageId: number
 ): AxiosPromise<void> =>
   axiosClient.delete(apiRoutes.POST.IMAGE(postId, imageId));
