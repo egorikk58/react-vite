5da64090ec76ebb1992be569d428e238e2a5ac7b
egorikk58 <egorka.andreev765@gmail.com>
2025-03-16 12:33:32 +0300

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import Auth from "@/page/auth.tsx";
+import {Routes, Route} from "react-router"
 import Lead from "./page/lead";
 import Login from "./components/auth/login";
 import { Navigate } from "react-router";
+import Register from "./components/auth/register";
 
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
-const App = () => {
-  return (
-    <Router>
-      <div className={"w-full h-full bg-slate-50"}>
-        <Routes>
-          <Route path="/auth/*" element={<Auth />} />
-          <Route path="/main/*" element={<Lead />} />
-          <Route
-            path="/"
-            element={
-              localStorage.getItem("accessToken")
-                ? <Navigate to="/main" replace />
-                : <Login />
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
-
-export default App;
+export default function App() {
+    return (
+        <div className={"w-full h-full bg-slate-50"}>
+            <Routes>
+                <Route path="auth">
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                </Route>
+                <Route path="/main/*" element={<Lead />} />
+                <Route 
+                    path="/" 
+                    element={
+                        localStorage.getItem("accessToken") 
+                            ? <Navigate to="/main" replace /> 
+                            : <Navigate to="/auth/login" replace /> 
+                    }
+                />
+            </Routes>
+        </div>
+    );
+}
